22b0a6a6c8b96945c911e5e42a9b0fd25fbce189
Ashik047 <ashikbiju2000@gmail.com>
2025-08-21 11:36:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl font-bold text-amber-600'>E-Cart</h1>
+    </>
+  )
+}
+
+export default App
